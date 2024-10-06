5bc74ccad361f24cac8003535e044ef7e42f63d0
yyman001 <yyman001@gmail.com>
2024-10-06 20:08:48 +0800

    css: 空内容提示剧中

--- src/renderer/src/pages/games/index.vue
+++ src/renderer/src/pages/games/index.vue
@@ -10,9 +10,8 @@ import { useGames } from './db'
 import { useConfigStoreWhitOut } from '@/store/config'
 import { storeToRefs } from 'pinia'
 import { callNodesync } from '@/utils/ipc'
-import { parseGameDocPath } from '@/utils/gameDocPath'
 
-const simpleImage = Empty.PRESENTED_IMAGE_SIMPLE
+const simpleImage = Empty.PRESENTED_IMAGE_DEFAULT
 const { backupPath } = storeToRefs(useConfigStoreWhitOut())
 
 // todo: 支持 搜索
@@ -81,6 +80,14 @@ const handleClickCard = ([appStatus, item]: CardEmitItem) => {
       onModalOpen()
       return
     case 'restore':
+      /*
+      lastFile = localFile.getLastFile(gameDocDir)
+          if (!lastFile) return error('未找到可以还原的备份存档文件!')
+          showRestoreFile(lastFile)
+      旧方案: 获取最新备份文件，并还原
+      新方案: 获取最近10个备份文件, 选中一个进行还原
+      新方案2: 跳转到���份文件列表页让用户自己选(可以把这个功能添加到右键菜单)
+      */
       return
     default:
       return
@@ -100,13 +107,13 @@ provide(modal, {
 </script>
 
 <template>
-  <div class="grid grid-rows-[auto, 1fr] h-screen w-full relative">
+  <div class="grid grid-rows-[auto,1fr,auto] h-screen w-full relative">
     <header class="h-24 p-4">
       <h2 class="text-2xl font-bold text-black">我的游戏库</h2>
     </header>
-    <main class="p-4 overflow-hidden bg-gray-100">
-      <ScrollArea class="w-full h-full border rounded-md config-page bg-slate-100">
-        <template v-if="GameItems?.length">
+    <main class="grid p-4 overflow-hidden bg-gray-100">
+      <template v-if="GameItems?.length">
+        <ScrollArea class="w-full h-full border rounded-md config-page bg-slate-100">
           <div class="grid grid-cols-4 gap-4">
             <Card
               :key="item.gameName"
@@ -119,11 +126,13 @@ provide(modal, {
               @contextmenu="onContextMenu($event, item)"
             />
           </div>
-        </template>
-        <template v-else>
+        </ScrollArea>
+      </template>
+      <template v-else>
+        <div class="grid h-full place-content-center">
           <Empty description="未找到游戏" :image="simpleImage" />
-        </template>
-      </ScrollArea>
+        </div>
+      </template>
     </main>
   </div>
   <ModalBackup />
